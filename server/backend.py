f1e70c08522d9db31bbe9d5026bdfd5f0d823627
Bhavik Arora <71212695+arorabhavik1001@users.noreply.github.com>
2025-04-18 01:03:04 +0530

    bug fix

--- server/backend.py
+++ server/backend.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -20,4 +21,5 @@ def ping():
     return "Pong"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
